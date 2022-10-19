232c8d49a9303193fce0fc14faa90ee4318fda6b
VictorM <vmmendozae123@gmail.com>
2022-10-19 11:41:08 -0500

    initial commit

---
+++ bin/global.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+let device = require('../src/index.js');
+
+device.init();
---
+++ src/index.js
@@ -0,0 +1,41 @@
+const deviceMixins = {
+    data() {
+        return {
+            windowWidth: 600,
+            interval: undefined
+        }
+    },
+    mounted() {
+        this.interval = setInterval(this.setListener(), 600)
+    },
+    computed: {
+        isMobile() {
+            return this.windowWidth <= 720
+        },
+        isTablet() {
+            return this.windowWidth > 720 && this.windowWidth <= 1112
+        },
+        isDesktop() {
+            return this.windowWidth > 1112
+        },
+        isMobileOrTablet() {
+            return this.isMobile || this.isTablet
+        },
+        isDesktopOrTablet() {
+            return this.isDesktop || this.isTablet
+        }
+    },
+    methods: {
+        setListener() {
+            if (window) {
+                this.windowWidth = window.innerWidth
+                window.addEventListener('resize', () => {
+                    this.windowWidth = window.innerWidth
+                })
+                clearInterval(this.interval)
+            }
+        }
+    }
+}
+
+export default deviceMixins
